4ef35a8e16704c32a7b93c94cac745a3538428af
samsad.valappil <samsad.valappil@thedigitalx.ae>
2023-01-03 23:36:37 +0400

    DatabaseModule created - provideDataBase and provideDao added

---
+++ app/src/main/java/com/samsad/composetodo/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.samsad.composetodo.di
+
+import android.content.Context
+import androidx.room.Room
+import com.samsad.composetodo.data.TodoDatabase
+import com.samsad.composetodo.ui.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * @Author: Samsad Chalil Valappil
+ * @Date: 03/01/2023
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDataBase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, TodoDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: TodoDatabase) = database.todoDao()
+}
