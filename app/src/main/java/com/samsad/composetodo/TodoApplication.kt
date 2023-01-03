ca0cdee73ea33daa777332b63f154994f5d3ab8b
samsad.valappil <samsad.valappil@thedigitalx.ae>
2023-01-03 23:29:24 +0400

    Added Hilt

---
+++ app/src/main/java/com/samsad/composetodo/TodoApplication.kt
@@ -0,0 +1,12 @@
+package com.samsad.composetodo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @Author: Samsad Chalil Valappil
+ * @Date: 03/01/2023
+ */
+@HiltAndroidApp
+class TodoApplication : Application() {
+}
