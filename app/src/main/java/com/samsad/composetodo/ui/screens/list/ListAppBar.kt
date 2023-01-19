984584fe7dd47ce4b62880b707336533b35a9b74
samsad.valappil <samsad.valappil@thedigitalx.ae>
2023-01-20 00:50:09 +0400

    Adding Serach ActionBar

--- app/src/main/java/com/samsad/composetodo/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/samsad/composetodo/ui/screens/list/ListAppBar.kt
@@ -1,21 +1,27 @@
 package com.samsad.composetodo.ui.screens.list
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import com.samsad.composetodo.R
 import com.samsad.composetodo.components.PriorityItem
 import com.samsad.composetodo.data.models.Priority
-import com.samsad.composetodo.ui.theme.LARGE_PADDING
-import com.samsad.composetodo.ui.theme.Typography
-import com.samsad.composetodo.ui.theme.topAppBarBgColor
-import com.samsad.composetodo.ui.theme.topAppBarContentColor
+import com.samsad.composetodo.ui.theme.*
 
 /**
  * @Author: Samsad Chalil Valappil
@@ -149,11 +155,100 @@ fun DeleteAll(
 }
 
 @Composable
+fun SerachAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSerachClicked: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        elevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colors.topAppBarBgColor
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = text,
+            onValueChange = {
+                onTextChange(it)
+            },
+            placeholder = {
+                Text(
+                    modifier = Modifier.alpha(ContentAlpha.medium),
+                    text = stringResource(R.string.search),
+                    color = Color.White
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colors.topAppBarContentColor,
+                fontSize = MaterialTheme.typography.subtitle1.fontSize
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    modifier = Modifier.alpha(ContentAlpha.disabled),
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = stringResource(id = R.string.search_icon),
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        onCloseClicked()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = stringResource(id = R.string.close_icon),
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSerachClicked(text)
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                cursorColor = MaterialTheme.colors.topAppBarContentColor,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                backgroundColor = Color.Transparent
+            )
+        )
+    }
+}
+
+
+@Composable
 @Preview
 fun DefaultListAppBarPreview() {
     DefaultListAppBar(
         onSerachClick = {},
         onSortClick = {},
         onDeleteClicked = {}
+    )
+}
+
+@Composable
+@Preview
+fun SerachAppBarPreview() {
+    SerachAppBar(
+        text = "Serach",
+        onTextChange = {},
+        onCloseClicked = {},
+        onSerachClicked = {}
     )
 }
