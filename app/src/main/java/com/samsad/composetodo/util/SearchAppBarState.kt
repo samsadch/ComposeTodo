c7ccef37da76bc769e21a851371e46cd20595798
samsad.valappil <samsad.valappil@thedigitalx.ae>
2023-01-20 22:24:26 +0400

    Created SearchAppBarState to hold the state

---
+++ app/src/main/java/com/samsad/composetodo/util/SearchAppBarState.kt
@@ -0,0 +1,11 @@
+package com.samsad.composetodo.util
+
+/**
+ * @Author: Samsad Chalil Valappil
+ * @Date: 20/01/2023
+ */
+enum class SearchAppBarState {
+    OPENED,
+    CLOSED,
+    TRIGGERED
+}
