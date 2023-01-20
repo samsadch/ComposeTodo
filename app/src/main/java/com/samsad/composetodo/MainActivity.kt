fcc6a0c8d76cbec9aa7d32559a6f013e5ff7252e
samsad.valappil <samsad.valappil@thedigitalx.ae>
2023-01-20 22:25:20 +0400

    sharedViewModel added

--- app/src/main/java/com/samsad/composetodo/MainActivity.kt
+++ app/src/main/java/com/samsad/composetodo/MainActivity.kt
@@ -3,22 +3,28 @@ package com.samsad.composetodo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.samsad.composetodo.navigation.SetupNavigation
 import com.samsad.composetodo.ui.theme.ComposeTodoTheme
+import com.samsad.composetodo.ui.viewmodels.SharedViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
+    private val sharedViewModel: SharedViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTodoTheme {
                 navController = rememberNavController()
-                SetupNavigation(navController = navController)
+                SetupNavigation(
+                    navController = navController,
+                    sharedViewModel = sharedViewModel
+                )
             }
         }
     }
