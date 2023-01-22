80b97ac6a4823d1e14d72503389126abac17e097
samsad.valappil <samsad.valappil@thedigitalx.ae>
2023-01-23 00:03:12 +0400

    Task Details Updates

---
+++ app/src/main/java/com/samsad/composetodo/ui/screens/task/TaskContent.kt
@@ -0,0 +1,86 @@
+package com.samsad.composetodo.ui.screens.task
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.samsad.composetodo.R
+import com.samsad.composetodo.components.PriorityDropDown
+import com.samsad.composetodo.data.models.Priority
+import com.samsad.composetodo.ui.theme.LARGE_PADDING
+import com.samsad.composetodo.ui.theme.MEDIUM_PADDING
+
+/**
+ * @Author: Samsad Chalil Valappil
+ * @Date: 22/01/2023
+ */
+
+@Composable
+fun TaskContent(
+    title: String,
+    onTitleChange: (String) -> Unit,
+    description: String,
+    onDescriptionChange: (String) -> Unit,
+    priority: Priority,
+    onPrioritySelected: (Priority) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+            .padding(all = LARGE_PADDING)
+
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = title,
+            onValueChange = { onTitleChange(it) },
+            label = {
+                Text(text = stringResource(R.string.title))
+            },
+            textStyle = MaterialTheme.typography.body1,
+            singleLine = true
+        )
+        Divider(
+            modifier = Modifier.height(MEDIUM_PADDING),
+            color = MaterialTheme.colors.background
+        )
+        PriorityDropDown(
+            priority = priority,
+            onPrioritySelected = onPrioritySelected
+        )
+        Divider(
+            modifier = Modifier.height(MEDIUM_PADDING),
+            color = MaterialTheme.colors.background
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxSize(),
+            value = description,
+            onValueChange = { onDescriptionChange(it) },
+            label = {
+                Text(text = stringResource(R.string.description))
+            },
+            textStyle = MaterialTheme.typography.body1,
+            singleLine = true
+        )
+    }
+}
+
+@Composable
+@Preview
+fun TaskContentPreview() {
+    TaskContent(
+        title = "Samsad",
+        onTitleChange = {},
+        description = "Test",
+        onDescriptionChange = {},
+        priority = Priority.HIGH,
+        onPrioritySelected = {}
+    )
+}
--- app/src/main/java/com/samsad/composetodo/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/samsad/composetodo/ui/screens/task/TaskScreen.kt
@@ -2,6 +2,7 @@ package com.samsad.composetodo.ui.screens.task
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.samsad.composetodo.data.models.Priority
 import com.samsad.composetodo.data.models.TodoTask
 import com.samsad.composetodo.util.Action
 
@@ -22,6 +23,27 @@ fun TaskScreen(
                 navigateToListScreen = navigateToListScreen
             )
         },
-        content = {}
+        content = {
+            if (selectedTask != null) {
+                TaskContent(
+                    title = selectedTask.title,
+                    onTitleChange = {},
+                    description = selectedTask.description,
+                    onDescriptionChange = {},
+                    priority = selectedTask.priority,
+                    onPrioritySelected = {}
+                )
+            } else {
+                TaskContent(
+                    title = "",
+                    onTitleChange = {},
+                    description = "",
+                    onDescriptionChange = {},
+                    priority = Priority.LOW,
+                    onPrioritySelected = {}
+                )
+            }
+
+        }
     )
 }
